f60ad064d877b1b9f014a8c2932702a740af50ca
MaksSadkov83 <maksraptor543@gmail.com>
2023-08-10 10:57:23 +0300

    Refactor of the main file and kv styles

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import time
 from kivy.clock import Clock
 from kivy.config import Config
 from kivymd.uix.menu import MDDropdownMenu
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 Config.set("graphics", "resizable", 0)
 Config.set("graphics", "width", 760)
 Config.set("graphics", "height", 700)
 
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.label import Label
 from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
 from kivymd.uix.filemanager import MDFileManager
 from kivymd.toast import toast
 from kivy.core.window import Window
@@ -20,29 +20,34 @@ import os
 from parcer_xlsx import ParcerXlsxData
 from threading import Thread
 
-Builder.load_file("frdostudent.kv")
 
-
-class MainWindow(MDScreen):
+class FrdoStudent(MDApp):
     res = 0
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        Window.bind(on_keyboard=self.events)
+    def __init__(self):
+        super().__init__()
         self.manager_open = False
 
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+
+        return Builder.load_file("frdostudent.kv")
+
     def ParcerLoad(self):
-        file_1 = self.ids.file1.text
-        file_2 = self.ids.file2.text
-        spec = self.ids.spec.text
+        file_1 = self.root.ids.file1
+        file_2 = self.root.ids.file2
+        spec = self.root.ids.spec
         file1 = list(file_1.split('\\'))
         file2 = list(file_2.split('\\'))
         if file_1 != "" and file_2 != "":
             if '.xlsx' in file1[-1] and '.xlsx' in file2[-1]:
                 if spec != "":
-                    self.manager.transition.direction = 'left'
-                    self.manager.current = 'Load'
-                    self.ParcerXlsxInfo(first_file=file_1, second_file=file_2, spec=spec)
+                    self.root.transition.direction = 'left'
+                    self.root.current = 'Load'
+                    self.ParcerXlsxInfo(first_file=file_1.text, second_file=file_2.text, spec=spec.text)
+                    file_1.text = ""
+                    file_2.text = ""
+                    spec.text = ""
                 else:
                     toast('Не выбрана специальность')
             else:
@@ -57,7 +62,7 @@ class MainWindow(MDScreen):
 
         def select_path(path: str):
             exit_manager()
-            self.ids.file1.text = path
+            self.root.ids.file1.text = path
 
         file_manager = MDFileManager(
             exit_manager=exit_manager, select_path=select_path
@@ -72,7 +77,7 @@ class MainWindow(MDScreen):
 
         def select_path(path: str):
             exit_manager()
-            self.ids.file2.text = path
+            self.root.ids.file2.text = path
 
         file_manager = MDFileManager(
             exit_manager=exit_manager, select_path=select_path
@@ -102,7 +107,7 @@ class MainWindow(MDScreen):
         ]
 
         menu = MDDropdownMenu(
-            caller=self.ids.button,
+            caller=self.root.ids.button,
             items=menu_items,
             width_mult=4,
         )
@@ -110,7 +115,7 @@ class MainWindow(MDScreen):
         menu.open()
 
     def text_spec(self, text):
-        self.ids.spec.text = text
+        self.root.ids.spec.text = text
 
     def ParcerXlsxInfo(self, first_file, second_file, spec):
         th1 = Thread(target=ParcerXlsxData, args=(first_file, second_file, spec))
@@ -122,39 +127,15 @@ class MainWindow(MDScreen):
             if self.res == 1:
                 self.res = 0
                 event.cancel()
-                ResultFrdoWindow().data(spec=spec)
-                self.manager.transition.direction = 'left'
-                self.manager.current = 'Result'
+                self.data(spec=spec)
+                self.root.transition.direction = 'left'
+                self.root.current = 'Result'
 
         event = Clock.schedule_interval(callback, 2)
 
-
-class LoadWindow(MDScreen):
-    pass
-
-
-class ResultFrdoWindow(MDScreen):
-    path = StringProperty()
-    name = StringProperty()
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        Window.bind(on_keyboard=self.events)
-
     def data(self, spec):
-        self.path = f'C:\\Users\\{os.getlogin()}\\Documents\\Сведения студентов из ФРДО {spec}.xlsx'
-        self.name = f"Сведения студентов из ФРДО {spec}.xlsx"
-
-
-class FrdoStudent(MDApp):
-    def build(self):
-        self.theme_cls.theme_style = "Dark"
-        sm = ScreenManager()
-        sm.add_widget(MainWindow())
-        sm.add_widget(LoadWindow())
-        sm.add_widget(ResultFrdoWindow())
-
-        return sm
+        self.root.ids.name.text = f"Файл сохранен под названием \n'Сведения студентов из ФРДО {spec}.xlsx'"
+        self.root.ids.path.text = f"Файл сохранен по пути \n'C:\\Users\\{os.getlogin()}\\Documents\\Сведения студентов из ФРДО {spec}.xlsx'"
 
 
 if __name__ == "__main__":
